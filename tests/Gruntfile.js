57706fe136849ab3293a35006f82c5a9bb0f5fce
rodzewich.oleg@gmail.com <RodZewCh1>
2014-12-02 22:35:15 +0300

    -

--- tests/Gruntfile.js
+++ tests/Gruntfile.js
@@ -2,25 +2,4 @@
 /*global module */
 
 module.exports = function (grunt) {
-
-    grunt.loadNpmTasks("grunt-contrib-uglify");
-
-    grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json"),
-        uglify: {
-            options: {
-                banner: grunt.file.read("src/banner.txt")
-            },
-            tasks: {
-                files: [{
-                    expand : false,
-                    dest   : "tasks/favicon.js",
-                    src    : "src/*.js"
-                }]
-            }
-        }
-    });
-
-    grunt.registerTask("default", "Build package.", ["uglify:tasks"]);
-
 };
